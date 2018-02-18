51514618ef9278a181860a0d0894ab2eafb27aef
Wiktor Grebla <greblus@gmail.com>
2018-02-18 12:03:12 +0100

    Add some stats and delay.

--- esp-idf/tasks_fun1/main/tasks_fun1.c
+++ esp-idf/tasks_fun1/main/tasks_fun1.c
@@ -2,6 +2,11 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+unsigned long cnt1 = 0, cnt2 = 0, cnt3 = 0;
+static const char *pcTextForTask1 = "Task 1 is running";
+static const char *pcTextForTask2 = "Task 2 is running";
+static const char *pcTextForTask3 = "Task 3 is running";
+
 void vTaskFunction( void *pvParameters )
 {
 	char *pcTaskName;
@@ -10,18 +15,22 @@ void vTaskFunction( void *pvParameters )
 	pcTaskName = ( char * ) pvParameters;
 	for( ;; )
 	{
-		/* Print out the name of this task. */
-		printf("%s on core %i\n", pcTaskName, xPortGetCoreID());
+		if (pcTaskName == pcTextForTask1)
+				cnt1++;
+		if (pcTaskName == pcTextForTask2)
+				cnt2++;
+		if (pcTaskName == pcTextForTask3)
+				cnt3++;
+
+		printf("%s on core %i. Stats: t1: %lu t2: %lu t3: %lu\n", pcTaskName, xPortGetCoreID(), cnt1, cnt2, cnt3);
 		fflush(stdout);
+	
+		vTaskDelay(20/portTICK_PERIOD_MS); // interestingly, if you make the delay smaller than 20ms it'll 
+										   // move tasks between CPU cores, if it's 20ms, they'll run on core 1
+										   // and if delay is too small, some tasks will cpu-starve the others
 	}
 }
 
-/* Define the strings that will be passed in as the task parameters. These are
-defined const and not on the stack to ensure they remain valid when the tasks are
-executing. */
-static const char *pcTextForTask1 = "Task 1 is running";
-static const char *pcTextForTask2 = "Task 2 is running";
-static const char *pcTextForTask3 = "Task 3 is running";
 
 void app_main()
 {
@@ -29,5 +38,5 @@ void app_main()
 	xTaskCreate( vTaskFunction, "Task 2", 2000, (void*)pcTextForTask2, 1, NULL );
 	xTaskCreate( vTaskFunction, "Task 3", 2000, (void*)pcTextForTask3, 1, NULL );
 	
-   	vTaskDelete(NULL);
+   	vTaskDelete(NULL); 
 }
