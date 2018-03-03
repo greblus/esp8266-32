739a358f2835f8e2ea6947246acd8059e8e46cea
Wiktor Grebla <greblus@gmail.com>
2018-03-03 15:56:25 +0100

    Simple android app with timer settings.

--- arduino-ide/wifiserv/onoff_java/app/src/main/java/com/test/greblus/button/MainActivity.java
+++ arduino-ide/wifiserv/onoff_java/app/src/main/java/com/test/greblus/button/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.BufferedInputStream;
@@ -14,7 +15,7 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean on = false;
+    boolean on = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,17 +24,11 @@ public class MainActivity extends AppCompatActivity {
     }
     public void onClick(View v) {
         final URL url;
-        Button button = findViewById(R.id.button);
         try {
-            if (on) {
-                url = new URL("http://192.168.1.7/gpio/0");
-                button.setText(R.string.but_off);
-            }
+            if (on)
+                url = new URL("http://10.0.1.60/gpio/0");
             else
-                {
-                url = new URL("http://192.168.1.7/gpio/1");
-                button.setText(R.string.but_on);
-            }
+                url = new URL("http://10.0.1.60/gpio/1");
             on = !on;
 
             Thread thread = new Thread() {
@@ -54,7 +49,36 @@ public class MainActivity extends AppCompatActivity {
             };
 
             thread.start();
-            Toast toast = Toast.makeText(MainActivity.this, "Wysłano!", Toast.LENGTH_SHORT);
+            Toast toast = Toast.makeText(MainActivity.this, "ON/OFF sent!", Toast.LENGTH_SHORT);
+            toast.show();
+        } catch (IOException ex) {}
+    }
+
+    public void setTimer(View v) {
+        final URL url;
+        try {
+            EditText etxt = findViewById(R.id.timeout);
+            url = new URL("http://10.0.1.60/timer/"+etxt.getText()+"&");
+
+            Thread thread = new Thread() {
+                @Override
+                public void run() {
+                    HttpURLConnection urlConnection = null;
+                    try {
+                        urlConnection = (HttpURLConnection) url.openConnection();
+                        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    finally {
+                        urlConnection.disconnect();
+                    }
+                }
+            };
+
+            thread.start();
+            Toast toast = Toast.makeText(MainActivity.this, "Timer set!", Toast.LENGTH_SHORT);
             toast.show();
         } catch (IOException ex) {}
     }
